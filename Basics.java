b61b5e2abde81ba3941379bbbda7e34efb6f43a7
chandalurisiva <sivachandaluri309@gmail.com>
2023-02-18 01:53:15 +0530

    First Commit

---
+++ Basics.java
@@ -0,0 +1,2 @@
+This file contains all the basics of java and 
+the execution starts from JVM(Java Vertual Machine)
---
+++ Frameworks.java
@@ -0,0 +1,3 @@
+This file contains all the freameworks 
+which is relevant to java and most popular softwares are
+Spring and springboot
